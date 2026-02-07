d609de3dd901216d61ff68bd585367a6f709f1c2
nicolas0898 <nicolascoutinhobr@gmail.com>
2026-02-07 14:56:35 -0300

    password

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 const list = document.getElementById("list")
 const dyr = document.getElementById("dyr")
 const fue = document.getElementById("fue")
+const password_input = document.getElementById("password")
+const block = document.getElementById("block")
+const password = "a1b2c3d4e5f6g7h8i9j10k11l12m13n14o15p16q17r18s19t20u21v22w23x24y25z26"
 
 chrome.storage.sync.get("redirectionURLS", function (item) {
     list.value = ""
@@ -33,3 +36,10 @@ list.addEventListener("change", ev => {
     chrome.storage.sync.set({ "redirectionURLS": [list.value.split(",")] })
 })
 
+password_input.addEventListener("input",()=>{
+    if(password_input.value == password){
+        block.classList.remove("d-none")
+    }else{
+        block.classList.add("d-none")
+    }
+})
