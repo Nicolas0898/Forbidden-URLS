fc0d57671e11673be996d864843d2bb5616708bc
nicolas0898 <nicolascoutinhobr@gmail.com>
2026-04-21 09:29:33 -0300

    better ui

---
+++ background.js
@@ -0,0 +1,82 @@
+var c_url = "https://google.com"
+var redirection_enabled = true
+
+var redirectPatterns = ["shorts"]
+
+function isPatternArrayInUrl(patternArray, Url) {
+
+  for (i in patternArray) {
+    let value = patternArray[i]
+
+    if(value[0]==":"){
+      let c =  (":"+ ( Url.split("/")[2] )) == value
+      if(c) return c 
+    }else{
+      if (Url.indexOf(value) != -1) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+function redirect(tab) {
+  if (!tab) return
+  if (redirection_enabled == false) return
+  console.log("QUERYING REDIRECTION")
+  if (isPatternArrayInUrl(redirectPatterns, tab.url)) {
+    // window.location.href = c_url;
+    // document.body.remove()
+    // document.head.remove()
+    // window.location.replace(c_url);
+    console.log(`REDIRECTION ${tab.url} to ${c_url}`)
+    chrome.tabs.update(tab.id,{url:c_url})
+  }
+}
+
+function loadData() {
+  chrome.storage.local.get(["fue","redirectionURLS"], function (item) {
+    redirection_enabled = item.fue
+
+    redirectPatterns = item.redirectionURLS
+  })
+
+
+}
+
+// navigation.addEventListener("navigate", e => {
+//   if (isPatternArrayInUrl(redirectPatterns, e.destination.url)) return
+//   loadData()
+// });
+
+loadData()
+
+chrome.tabs.onActivated.addListener(async obj=>{
+  console.log("activated tab")
+  const tab = await chrome.tabs.get(obj.tabId)
+  console.log(tab)
+  // console.log(window)
+  // console.log(chrome.tabs.pendingUrl)
+  redirect(tab)
+})
+
+chrome.tabs.onUpdated.addListener(async tabId=>{
+  console.log("updated tab")
+  const tab = await chrome.tabs.get(tabId)
+  console.log(tab)
+
+  redirect(tab)
+})
+
+// checks for changes
+chrome.storage.onChanged.addListener((changes) => {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    if (key == "fue") {
+      redirection_enabled = newValue
+    }
+    if (key == "redirectionURLS") {
+      redirectPatterns = newValue
+    }
+  }
+});
--- src/data.js
+++ src/data.js
@@ -1,21 +1,66 @@
 const list = document.getElementById("list")
+const scrollList = document.getElementById("scrollList")
 const dyr = document.getElementById("dyr")
 const fue = document.getElementById("fue")
 const password_input = document.getElementById("password")
 const block = document.getElementById("block")
-const password = "a1b2c3d4e5f6g7h8i9j10k11l12m13n14o15p16q17r18s19t20u21v22w23x24y25z26"
+const addBtn   = document.getElementById("addBtn")
+const addInput = document.getElementById("addInput")
+const password = "9876543210abcdefghijklmnopqrstuvwxyz0123456789"
 
-chrome.storage.sync.get("redirectionURLS", function (item) {
-    list.value = ""
-    for (i in item) {
-        let element = item[i]
-        list.value += element.toString() + ","
+
+function createWebsiteWidget(name){
+    let div = document.createElement("div")
+    div.className = "border rounded p-3 m-1 d-flex justify-content-between px-2"
+
+    let span = document.createElement("span")
+    span.innerHTML = name
+    // span.className = "fw-bold"
+
+    let btn = document.createElement("button")
+    btn.className = "btn-close"
+    btn.addEventListener("click",()=>{
+        removeElement(name)
+    })
+
+    div.appendChild(span)
+    div.appendChild(btn)
+
+    return div
+}
+
+async function removeElement(element){
+    const values = (await chrome.storage.local.get("redirectionURLS")).redirectionURLS
+    var result = []
+
+
+    for(i in values){
+        if(values[i]!=element) result.push(values[i])
     }
+
+    chrome.storage.local.set({"redirectionURLS":result})
+    updateList(result)
+}
+
+function updateList(values){
+    scrollList.innerHTML = ''
+    for (i in values) {
+        console.log(`i: ${i}\nalues[i]:${values[i]}`)
+        let element = values[i]
+        let div = createWebsiteWidget(element)
+        scrollList.appendChild(div)
+    }
+}
+
+chrome.storage.local.get("redirectionURLS", function (item) {
+    // list.value = ""
+    const values = item.redirectionURLS
+    updateList(values)
 });
 
 function regBool(name) {
     let node = document.getElementById(name)
-    chrome.storage.sync.get(name, function (value) {
+    chrome.storage.local.get(name, function (value) {
         if (value[name] == null)
             node.checked = true
 
@@ -25,15 +70,24 @@ function regBool(name) {
     node.addEventListener("change", ev => {
         let data = {}
         data[name] = node.checked
-        chrome.storage.sync.set(data)
+        chrome.storage.local.set(data)
     })
 }
 
 regBool("fue")
 regBool("dyr")
 
-list.addEventListener("change", ev => {
-    chrome.storage.sync.set({ "redirectionURLS": [list.value.split(",")] })
+// list.addEventListener("change", ev => {
+//     chrome.storage.local.set({ "redirectionURLS": [list.value.split(",")] })
+// })
+
+addBtn.addEventListener("click",async ()=>{
+    const values = (await chrome.storage.local.get("redirectionURLS")).redirectionURLS
+    console.log(values)
+    values.push(addInput.value)
+    addInput.value = ''
+    chrome.storage.local.set({"redirectionURLS":values})
+    updateList(values)
 })
 
 password_input.addEventListener("input",()=>{
